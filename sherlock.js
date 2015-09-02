db11b787239fdbf95e7ba1a87febb9b8449b5077
dominichermidass <dominic_hermida@hotmail.com>
2015-09-01 21:06:08 -0500

    Update sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,30 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+var i=1;
+function sherlock(INPUT){
+split1 = INPUT.split("\n");
+casos = split1[0];
+
+
+if (casos>=1 && casos<= 100){
+while (i <= casos){
+contador = 0;
+split2 = split1[i].split(" ");
+			
+		for(j=split2[0];j<=split2[1];j++){
+			if(Math.sqrt(j) % 1 == 0){
+				contador =  contador + 1;
+					}		
+
+}
+console.log(contador);
+i = i +1;
+}
+}
+
+else {
+console.log("EL numero de casos tiene que ser mayor a 0 y menor a 101");
+}
+}
+sherlock("6\n1 8\n2 4\n10 13\n11 20\n1 8\n1 8");
